cbe982996d5c40f46f01c994518cca1162afb7f5
kamali <93373518+kamalikumar123@users.noreply.github.com>
2021-12-18 14:53:06 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,13 @@
+package gettingobjectoriented;
+
+import java.util.*;
+
+public class GettingObjectOriented {
+    public static void main(String[] args) {
+            Person john = new Person();
+            john.firstName = "sneha";
+            john.lastName = "kamali";
+            john.birthday = new GregorianCalendar(1988,1,5);
+            
+            System.out.println(kamali.age(new GregorianCalendar()));           
+    }
